e19446c751441e2b174dba86daf3271194905846
JW0203 <widwoo0203@gmail.com>
2024-11-08 14:56:47 -0800

    feat: add mongoose

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const PORT = 8080;
 const HOST = '0.0.0.0';
 const app = express();
 const productRoutes = require('./routes');
+const mongoose = require('mongoose');
+
+mongoose.connect(
+    'mongodb+srv://widwoo0203:4258@cluster0.miwva.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+)
+    .then(() => console.log('MongoDb Connected...'))
+    .catch(err => console.log(err));
 
 app.use(express.json());
 app.use("/api/products", productRoutes);
