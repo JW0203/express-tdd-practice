76a90df1e203eec79a6d847ff8fe93d2d99df134
JW0203 <widwoo0203@gmail.com>
2024-11-07 16:59:46 -0800

    feat: add routes to handling api requests api/products

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const PORT = 8080;
 const HOST = '0.0.0.0';
 const app = express();
+const productRoutes = require('./routes');
 
 app.use(express.json());
+app.use("/api/products", productRoutes);
 
 app.get('/', (req, res) => {
     res.send('Welcome to my website!');
