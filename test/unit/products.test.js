3c6af0566050d393fffb56e67787868faa8d8369
JW0203 <widwoo0203@gmail.com>
2024-11-13 22:15:58 +0900

    feat: addrequest body

--- test/unit/products.test.js
+++ test/unit/products.test.js
@@ -1,6 +1,7 @@
 const productController = require("../../controller/products");
 const productModel = require("../../models/Product");
 const httpMocks = require('node-mocks-http');
+const newProduct = require('../data/new-product.json');
 
 productModel.create = jest.fn();
 
@@ -12,7 +13,7 @@ describe('Products Controller', ()=>{
         let req = httpMocks.createRequest();
         let res = httpMocks.createResponse();
         let next = null;
-
+        req.body = newProduct;
         productController.createProduct(req, res, next);
         expect(productModel.create).toBeCalled();
     })
