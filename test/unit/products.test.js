8f0d89e52ac7cf2e893b9d8d6af0cc5f10392df8
JW0203 <widwoo0203@gmail.com>
2024-11-13 20:09:31 +0900

    feat: pass a test

--- test/unit/products.test.js
+++ test/unit/products.test.js
@@ -1,3 +1,6 @@
+const productController = require("../../controller/products");
+
+
 describe('Products Controller', ()=>{
     it('Should have a createdProduct function', ()=>{
         expect(typeof productController.createProduct).toBe('function');
