051d07728a361d9000e460eeed48236c6ad63280
JW0203 <widwoo0203@gmail.com>
2024-11-13 20:12:33 +0900

    fea: make a test code for productModel.create

--- test/unit/products.test.js
+++ test/unit/products.test.js
@@ -1,8 +1,13 @@
 const productController = require("../../controller/products");
-
+const productModel = require("../../models/Product");
 
 describe('Products Controller', ()=>{
     it('Should have a createdProduct function', ()=>{
         expect(typeof productController.createProduct).toBe('function');
+    })
+    it ('Should call ProductModel.create', () => {
+        productController.createProduct();
+        expect(productModel.create).toBeCalled();
+
     })
 })
