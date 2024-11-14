36eddb03572908a3cd0d5d5dc42bf617c459d875
JW0203 <widwoo0203@gmail.com>
2024-11-14 11:00:28 +0900

    feat: implement codes to handle error case

--- controller/products.js
+++ controller/products.js
@@ -1,6 +1,10 @@
 const productModel = require('../models/Product');
 
 exports.createProduct = async (req, res, next) => {
-    const createdProduct = await productModel.create(req.body);
-    res.status(201).json(createdProduct);
+    try{
+        const createdProduct = await productModel.create(req.body);
+        res.status(201).json(createdProduct);
+    }catch (error) {
+        next(error); // next 에 에러값을 넣어주면 비동기 요청에 대한 에러를 잘처리 할 수 있는 곳으로 보내준다.
+    }
 };
